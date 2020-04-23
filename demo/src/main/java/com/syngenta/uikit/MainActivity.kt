636ae3775be957c0fdfe5e7fc1525fca820a2a0a
Sagar <sagar.mungole@syngenta.com>
2020-04-23 15:39:15 +0530

    Different color and style support

--- demo/src/main/java/com/syngenta/uikit/MainActivity.kt
+++ demo/src/main/java/com/syngenta/uikit/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.syngenta.uikit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.syngenta.uikit_android.StyledButton
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectTeal.setOnClickListener {
+            clearAllChecks()
+            selectTeal.setImageResource(R.drawable.ic_check)
+            setButtonColor(StyledButton.ColorGroup.TEAL)
+        }
+
+        selectBlue.setOnClickListener {
+            clearAllChecks()
+            selectBlue.setImageResource(R.drawable.ic_check)
+            setButtonColor(StyledButton.ColorGroup.BLUE)
+        }
+
+        selectYellow.setOnClickListener {
+            clearAllChecks()
+            selectYellow.setImageResource(R.drawable.ic_check)
+            setButtonColor(StyledButton.ColorGroup.YELLOW)
+        }
+
+        selectRed.setOnClickListener {
+            clearAllChecks()
+            selectRed.setImageResource(R.drawable.ic_check)
+            setButtonColor(StyledButton.ColorGroup.RED)
+        }
+
+        selectOrange.setOnClickListener {
+            clearAllChecks()
+            selectOrange.setImageResource(R.drawable.ic_check)
+            setButtonColor(StyledButton.ColorGroup.ORANGE)
+        }
+    }
+
+    private fun setButtonColor(color: StyledButton.ColorGroup) {
+        primaryButton.setAccentColor(color)
+        secondaryButton.setAccentColor(color)
+        tertiaryButton.setAccentColor(color)
+        textButton.setAccentColor(color)
+    }
+
+    private fun clearAllChecks() {
+        selectTeal.setImageResource(android.R.color.transparent)
+        selectBlue.setImageResource(android.R.color.transparent)
+        selectYellow.setImageResource(android.R.color.transparent)
+        selectRed.setImageResource(android.R.color.transparent)
+        selectOrange.setImageResource(android.R.color.transparent)
     }
 }
--- uikit-android/src/main/java/com/syngenta/uikit_android/Buttons.kt
+++ uikit-android/src/main/java/com/syngenta/uikit_android/Buttons.kt
@@ -2,10 +2,24 @@ package com.syngenta.uikit_android
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.MotionEvent
 import android.widget.Button
 
 class StyledButton : Button {
+
+    enum class ColorGroup {
+        TEAL,
+        ORANGE,
+        RED,
+        BLUE,
+        YELLOW
+    }
+
+    enum class ButtonType {
+        PRIMARY,
+        SECONDARY,
+        TERTIARY,
+        TEXT
+    }
 
     var colorGroup: Int = 0
     var type: Int = 0
@@ -14,46 +28,91 @@ class StyledButton : Button {
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.StyledButton)
-
-        colorGroup = typedArray.getInt(R.styleable.StyledButton_primaryColor, 0)
+        colorGroup = typedArray.getInt(R.styleable.StyledButton_accent, 0)
         type = typedArray.getInt(R.styleable.StyledButton_type, 0)
-        this.setTextColor(resources.getColor(getTextColor(), null))
-        this.setBackgroundColor(resources.getColor(getBackgroundColor(), null))
+        refreshTheme()
+        typedArray.recycle()
     }
 
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                this.setBackgroundColor(resources.getColor(getPressedColor(), null))
-            }
-
-            MotionEvent.ACTION_UP -> {
-                this.setBackgroundColor(resources.getColor(getBackgroundColor(), null))
-            }
+    fun setAccentColor(color: ColorGroup) {
+        colorGroup = when (color) {
+            ColorGroup.TEAL -> 0
+            ColorGroup.ORANGE -> 1
+            ColorGroup.RED -> 2
+            ColorGroup.BLUE -> 3
+            ColorGroup.YELLOW -> 4
         }
-        return super.onTouchEvent(event)
+        refreshTheme()
     }
 
-    private fun getBackgroundColor(): Int {
+    fun setType(buttonType: ButtonType) {
+        type = when (buttonType) {
+            ButtonType.PRIMARY -> 0
+            ButtonType.SECONDARY -> 1
+            ButtonType.TERTIARY -> 2
+            ButtonType.TEXT -> 3
+        }
+        refreshTheme()
+    }
+
+    private fun refreshTheme() {
+        this.setTextColor(resources.getColor(getTextColor()))
+        this.background = resources.getDrawable(getBackgroundRes())
+    }
+
+    private fun getBackgroundRes(): Int {
         return when (colorGroup) {
-            0 -> R.color.teal_500
-            1 -> R.color.orange_500
-            2 -> R.color.red_500
-            3 -> R.color.blue_500
-            4 -> R.color.yellow_500
-            else -> R.color.teal_500
+            0 -> getTealTheme()
+            1 -> getOrangeTheme()
+            2 -> getRedTheme()
+            3 -> getBlueTheme()
+            4 -> getYellowTheme()
+            else -> getTealTheme()
         }
     }
 
-    private fun getPressedColor(): Int {
-        return when (colorGroup) {
-            0 -> R.color.teal_700
-            1 -> R.color.orange_700
-            2 -> R.color.red_700
-            3 -> R.color.blue_700
-            4 -> R.color.yellow_700
-            else -> R.color.teal_700
+    private fun getTealTheme(): Int {
+        return when (type) {
+            0 -> R.drawable.bg_teal_primary
+            1 -> R.drawable.bg_teal_secondary
+            2 -> R.drawable.bg_teal_tertiary
+            else -> R.drawable.bg_text
+        }
+    }
+
+    private fun getOrangeTheme(): Int {
+        return when (type) {
+            0 -> R.drawable.bg_orange_primary
+            1 -> R.drawable.bg_orange_secondary
+            2 -> R.drawable.bg_orange_tertiary
+            else -> R.drawable.bg_text
+        }
+    }
+
+    private fun getRedTheme(): Int {
+        return when (type) {
+            0 -> R.drawable.bg_red_primary
+            1 -> R.drawable.bg_red_secondary
+            2 -> R.drawable.bg_red_tertiary
+            else -> R.drawable.bg_text
+        }
+    }
+
+    private fun getBlueTheme(): Int {
+        return when (type) {
+            0 -> R.drawable.bg_blue_primary
+            1 -> R.drawable.bg_blue_secondary
+            2 -> R.drawable.bg_blue_tertiary
+            else -> R.drawable.bg_text
+        }
+    }
+
+    private fun getYellowTheme(): Int {
+        return when (type) {
+            0 -> R.drawable.bg_yellow_primary
+            1 -> R.drawable.bg_yellow_secondary
+            2 -> R.drawable.bg_yellow_tertiary
+            else -> R.drawable.bg_text
         }
     }
 
