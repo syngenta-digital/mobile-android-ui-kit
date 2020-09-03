aac24abc2f456163c77eebb81becab4fadb4e28b
Sagar <sagar.mungole@syngenta.com>
2020-09-03 15:14:59 +0530

    Cropwise colors

--- demo/src/main/java/com/syngenta/uikit/fragments/ColorsFragment.kt
+++ demo/src/main/java/com/syngenta/uikit/fragments/ColorsFragment.kt
@@ -42,12 +42,6 @@ class ColorsFragment : Fragment() {
         val recyclerView = recycler_color_list
         val layoutManager = LinearLayoutManager(context)
         recyclerView.layoutManager = layoutManager
-        recyclerView.addItemDecoration(
-            DividerItemDecoration(
-                recyclerView.context,
-                layoutManager.orientation
-            )
-        )
         val adapter = ColorsAdapter(context, colorList.toTypedArray())
         recyclerView.adapter = adapter
 
@@ -55,28 +49,41 @@ class ColorsFragment : Fragment() {
 
     private fun initializeList() {
         colorList.clear()
-        colorList.add(ColorItem("Teal 100", R.color.teal_100))
-        colorList.add(ColorItem("Teal 300", R.color.teal_300))
-        colorList.add(ColorItem("Teal 500", R.color.teal_500))
-        colorList.add(ColorItem("Teal 700", R.color.teal_700))
-        colorList.add(ColorItem("Orange 100", R.color.orange_100))
-        colorList.add(ColorItem("Orange 300", R.color.orange_300))
-        colorList.add(ColorItem("Orange 500", R.color.orange_500))
-        colorList.add(ColorItem("Orange 700", R.color.orange_700))
-        colorList.add(ColorItem("Red 100", R.color.red_100))
-        colorList.add(ColorItem("Red 300", R.color.red_300))
-        colorList.add(ColorItem("Red 500", R.color.red_500))
-        colorList.add(ColorItem("Red 700", R.color.red_700))
-        colorList.add(ColorItem("Blue 100", R.color.blue_100))
-        colorList.add(ColorItem("Blue 300", R.color.blue_300))
-        colorList.add(ColorItem("Blue 500", R.color.blue_500))
-        colorList.add(ColorItem("Blue 700", R.color.blue_700))
-        colorList.add(ColorItem("Yellow 100", R.color.yellow_100))
-        colorList.add(ColorItem("Yellow 300", R.color.yellow_300))
-        colorList.add(ColorItem("Yellow 500", R.color.yellow_500))
-        colorList.add(ColorItem("Yellow 700", R.color.yellow_700))
-        colorList.add(ColorItem("Neutral 500", R.color.neutral_500))
+        colorList.add(ColorItem("Green 50 (cw_green_50)", R.color.cw_green_50))
+        colorList.add(ColorItem("Green 60 (cw_green_60)", R.color.cw_green_60))
+        colorList.add(ColorItem("Green 70 (cw_green_70)", R.color.cw_green_70))
 
+        colorList.add(ColorItem("Red 50 (cw_red_50)", R.color.cw_red_50))
+        colorList.add(ColorItem("Red 60 (cw_red_60)", R.color.cw_red_60))
+        colorList.add(ColorItem("Red 70 (cw_red_70)", R.color.cw_red_70))
+
+        colorList.add(ColorItem("Orange 50 (cw_orange_50)", R.color.cw_orange_50))
+        colorList.add(ColorItem("Orange 60 (cw_orange_60)", R.color.cw_orange_60))
+        colorList.add(ColorItem("Orange 70 (cw_orange_70)", R.color.cw_orange_70))
+
+        colorList.add(ColorItem("Yellow 50 (cw_yellow_50)", R.color.cw_yellow_50))
+        colorList.add(ColorItem("Yellow 60 (cw_yellow_60)", R.color.cw_yellow_60))
+        colorList.add(ColorItem("Yellow 70 (cw_yellow_70)", R.color.cw_yellow_70))
+
+        colorList.add(ColorItem("Blue 50 (cw_blue_50)", R.color.cw_blue_50))
+        colorList.add(ColorItem("Blue 60 (cw_blue_60)", R.color.cw_blue_60))
+        colorList.add(ColorItem("Blue 70 (cw_blue_70)", R.color.cw_blue_70))
+
+        colorList.add(ColorItem("Teal 50 (cw_teal_50)", R.color.cw_teal_50))
+        colorList.add(ColorItem("Teal 60 (cw_teal_60)", R.color.cw_teal_60))
+        colorList.add(ColorItem("Teal 70 (cw_teal_70)", R.color.cw_teal_70))
+
+        colorList.add(ColorItem("Purple 50 (cw_purple_50)", R.color.cw_purple_50))
+        colorList.add(ColorItem("Purple 60 (cw_purple_60)", R.color.cw_purple_60))
+        colorList.add(ColorItem("Purple 70 (cw_purple_70)", R.color.cw_purple_70))
+
+        colorList.add(ColorItem("Pink 50 (cw_pink_50)", R.color.cw_pink_50))
+        colorList.add(ColorItem("Pink 60 (cw_pink_60)", R.color.cw_pink_60))
+        colorList.add(ColorItem("Pink 70 (cw_pink_70)", R.color.cw_pink_70))
+
+        colorList.add(ColorItem("Neutral 50 (cw_neutral_50)", R.color.cw_neutral_50))
+        colorList.add(ColorItem("Neutral 60 (cw_neutral_60)", R.color.cw_neutral_60))
+        colorList.add(ColorItem("Neutral 70 (cw_neutral_70)", R.color.cw_neutral_70))
     }
 
     companion object {
